487d7523ce0587f166421782db83f9df9653ede5
Kevin Nagurski <kevin.nagurski@bazaarvoice.com>
2017-12-13 13:37:26 +0000

    Add try again when response not heard

--- src/core/SpeechRecognition.js
+++ src/core/SpeechRecognition.js
@@ -13,15 +13,39 @@ function getSpeechRecognition () {
   return recognition
 }
 
+function getRandomCantHearPhrase () {
+  const phrases = [
+    'Sorry, I didn\'t hear that',
+    'What was that?',
+    'Whoops, I wasn\'t paying attention. What did you say?',
+    'All I heard was blah blah blah. Say again',
+    'Hey, is this thing on?',
+    'I feel like you might be ignoring me',
+  ]
+
+  return phrases[Math.floor(Math.random() * phrases.length)]
+}
+
 export function listen () {
   return new Promise(resolve => {
     event.$emit('listen:start')
     console.log('Starting to listen')
     recognition.start()
 
+    let heardAnything = false
+    setTimeout(async function () {
+      if (!heardAnything) {
+        recognition.stop()
+        recognition.removeEventListener('result', transcribe)
+        event.$emit('listen:stop')
+        resolve(speak(getRandomCantHearPhrase()).then(listen))
+      }
+    }, 10000)
+
     function transcribe ({results}) {
       const phrase = [...results].map(result => result[0]).map(result => result.transcript).join('')
 
+      heardAnything = true
       event.$emit('listen:heard', phrase)
 
       if (results[0].isFinal) {
